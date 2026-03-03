ed46d412ec093398eb93e2e021f30de6b1a6f68f
LazerzX <mixkung2549@gmail.com>
2026-03-03 12:15:51 +0700

    Area detail

--- myproject/src/main/java/com/project/Model/Area.java
+++ myproject/src/main/java/com/project/Model/Area.java
@@ -3,12 +3,35 @@ package com.project.Model;
 public class Area {
 
     private String name;
+    private int tripCount;
+    private double totalRevenue;
+    private double avgFuelConsumption;
 
-    public Area(String name) {
+    public Area(String name, int tripCount, double totalRevenue, double avgFuelConsumption) {
         this.name = name;
+        this.tripCount = tripCount;
+        this.totalRevenue = totalRevenue;
+        this.avgFuelConsumption = avgFuelConsumption;
     }
 
     public String getName() {
         return name;
+    }
+
+    public int getTripCount() {
+        return tripCount;
+    }
+
+    public double getTotalRevenue() {
+        return totalRevenue;
+    }
+
+    public double getAvgFuelConsumption() {
+        return avgFuelConsumption;
+    }
+
+    public double getRevenuePerTrip() {
+        if (tripCount == 0) return 0;
+        return totalRevenue / tripCount;
     }
 }
